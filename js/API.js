b135047216c41f5e474b0cd23a6de7600bc448ab
JulianCode <hijuliansosa@protonmail.com>
2020-05-23 11:18:51 -0500

    Adding the Pins. From API to Map

--- js/API.js
+++ js/API.js
@@ -1,6 +1,6 @@
 class API {
     async obtenerDatos () {
-        const total = 11000;
+        const total = 1000;
         const datos = await fetch(`https://api.datos.gob.mx/v1/precio.gasolina.publico?pageSize=${total}`);
 
         const respuestaJSON = await datos.json();
--- js/UI.js
+++ js/UI.js
@@ -3,31 +3,49 @@ class UI {
     // Instanciar la API
     this.api = new API()
 
+    // Crear los markers con LayerGroup
+    this.markers = L.layerGroup()
+
     // Iniciar el mapa
-    this.mapa = this.inicializarMapa();
+    this.mapa = this.inicializarMapa()
   }
 
   inicializarMapa() {
        // Inicializar y obtener la propiedad del mapa
-       const map = L.map('mapa').setView([19.390519, -99.3739778], 6);
-       const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+       const map = L.map('mapa').setView([19.390519, -99.3739778], 6)
+       const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>'
        L.tileLayer(
            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-           attribution: '&copy; ' + enlaceMapa + ' Contributors',
+           attribution: '&copy ' + enlaceMapa + ' Contributors',
            maxZoom: 18,
-           }).addTo(map);
-       return map;
+           }).addTo(map)
+       return map
 
   }
   async mostrarEstablecimientos() {
     this.api.obtenerDatos()
       .then(data => {
-        const resultados = data.respuestaJSON.results;
+        const resultados = data.respuestaJSON.results
         // Ejecutar función para mostrar PINES
         this.mostrarPines(resultados)
       })
   }
   mostrarPines(datos) {
+    // Limpiar los markers siempre que se manden llamar
+    this.markers.clearLayers()
+
+    datos.forEach(dato => {
+      const { latitude, longitude, calle, regular, premium } = dato
+
+      // Agregar PIN
+      const marker = new L.marker([
+        parseFloat(latitude),
+        parseFloat(longitude)
+      ])
+      // Agregar el pin al grupo de markers
+      this.markers.addLayer(marker)
+    })
+    this.markers.addTo(this.mapa)
     console.log(datos)
   }
 }
